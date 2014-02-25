d1dee3bffc447517ea6c0cc717e0f57117b846d8
HelenFoster <0904275f@student.gla.ac.uk>
2014-02-25 15:33:50 +0000

    Added stub templates and referenced them from views.

--- playmaker_project/playmaker/views.py
+++ playmaker_project/playmaker/views.py
@@ -11,43 +11,43 @@ from models import *
 def login(request):
     context = RequestContext(request)
     context_dict = {'your_key': 'your_value'}
-    return render_to_response('index.html', context_dict, context)
+    return render_to_response('login.html', context_dict, context)
 
 @csrf_exempt
 def register(request):
     context = RequestContext(request)
     context_dict = {'your_key': 'your_value'}
-    return render_to_response('index.html', context_dict, context)
+    return render_to_response('register.html', context_dict, context)
 
 @csrf_exempt
 def bookings(request):
     context = RequestContext(request)
     context_dict = {'your_key': 'your_value'}
-    return render_to_response('index.html', context_dict, context)
+    return render_to_response('bookings.html', context_dict, context)
 
 @csrf_exempt
 def preferences(request):
     context = RequestContext(request)
     context_dict = {'your_key': 'your_value'}
-    return render_to_response('index.html', context_dict, context)
+    return render_to_response('preferences.html', context_dict, context)
 
 @csrf_exempt
 def user_profile(request, username):
     context = RequestContext(request)
     context_dict = {'your_key': 'your_value'}
-    return render_to_response('index.html', context_dict, context)
+    return render_to_response('user_profile.html', context_dict, context)
 
 @csrf_exempt
 def view_sessions(request):
     context = RequestContext(request)
     context_dict = {'your_key': 'your_value'}
-    return render_to_response('index.html', context_dict, context)
+    return render_to_response('view_sessions.html', context_dict, context)
 
 @csrf_exempt
 def view_session_by_id(request, session_id):
     context = RequestContext(request)
     context_dict = {'your_key': 'your_value'}
-    return render_to_response('index.html', context_dict, context)
+    return render_to_response('view_session_by_id.html', context_dict, context)
 
 @csrf_exempt
 def view_sessions_by_sport(request, session_sport):
@@ -56,4 +56,4 @@ def view_sessions_by_sport(request, session_sport):
     session_list = Session.objects.filter(sport=session_sport)
     print len(session_list)
     context_dict = {'sport': session_sport, 'sessions': session_list}
-    return render_to_response('sessionsBySport.html', context_dict, context)
+    return render_to_response('view_sessions_by_sport.html', context_dict, context)
