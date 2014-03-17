7a2d36343da2d34cb54262ea4a5b511ddbca6b70
Martynas <martynas.buivys@gmail.com>
2014-03-17 19:45:44 +0000

    AJAX base implementation

--- playmaker_project/static/js/scripts.js
+++ playmaker_project/static/js/scripts.js
@@ -6,7 +6,28 @@ $(document).ready(function() {
     // Adjust sidebar height
     var preferedHeight = $(window).height() - $(".header").css("height").slice(0,-2);
     $(".sidebar-wrap").css("height", preferedHeight);
-    //$(".discussion").css("height", preferedHeight)
+
+    // Send messages via AJAX
+    $(".chat-message-send").click(function() {
+
+        var message = $(".chat-message").val();
+
+        // TODO: pick up CSRF token and ship it along with ajax request
+        if (message.trim() != "") {
+
+            $.post("/sendmsg/", {
+                data: message
+            }).done(function() {
+                alert("Message sent successfully!");
+                reloadMessages();
+            }).fail(function() {
+                alert("Message sending failure!");
+            });
+        }
+
+    });
+
+
 
 });
 
@@ -18,10 +39,32 @@ $(document).ready(function() {
 /*
  *	Make DIVs of the same height
  *	Usage: $(‘div.unevenheights’).setAllToMaxHeight();
- *  Thanks Paul Irish
+ *  Thanks @Paul Irish
  */
 
 $.fn.setAllToMaxHeight = function(){
-    console.log(this);
 	return this.height(Math.max.apply(this,$.map(this,function(e){return $(e).height()})));
+}
+
+function reloadMessages() {
+
+    var $target = $(".discussion");
+    var url = "/getmessages/";
+    var messages = [];
+
+    // Retrieve all messages for the discussion
+    $.post(url, {
+        sessionId: "1"
+    }).done(function(data) {
+        alert("Successfully got the messages! ");
+
+        // Clear current messages
+        $target.empty();
+
+        // Load the new messages
+        $target.append(data);
+    }).fail(function() {
+        alert("Could not reload messages!");
+    });
+
 }
