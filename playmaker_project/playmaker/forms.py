ed4567dc279e4d4b5fdc6f8a4575c505bb6fc338
ralucaa <1102430c@student.gla.ac.uk>
2014-03-17 20:49:52 +0000

    made registration work

--- playmaker_project/playmaker/forms.py
+++ playmaker_project/playmaker/forms.py
@@ -4,17 +4,18 @@ from django.contrib.auth.models import User
 from models import *
 from django.core.exceptions import ValidationError
 
-
 class RegistrationForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=64)))
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=64)))
-    first_name = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=64)))
-    last_name = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=64)))
-    password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=64, render_value=False)))
+    username = forms.CharField()
+    email = forms.EmailField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    password = forms.CharField()
+    city = forms.CharField()
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password')
+
+    #class Meta:
+        #model = User
+        #fields = ('username', 'email', 'first_name', 'last_name', 'password', 'city')
 
     def clean_username(self):
         try:
@@ -22,6 +23,9 @@ class RegistrationForm(forms.Form):
         except User.DoesNotExist:
             return self.cleaned_data['username']
         raise forms.ValidationError("The username already exists. Please try another one.")
+
+
+
 
 
 class PreferencesForm(forms.Form):
